3bf54a38d23a7e5ee3b31d46e9118d5b30e07fa4
netoudi <netoudi@rocketmail.com>
2023-06-27 16:04:58 -0300

    feat(backend): add prisma module

---
+++ backend/src/prisma/prisma.service.ts
@@ -0,0 +1,19 @@
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  async onModuleInit() {
+    try {
+      await this.$connect();
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async enableShutdownHooks(app: any) {
+    this.$on('beforeExit', async () => {
+      await app.close();
+    });
+  }
+}
